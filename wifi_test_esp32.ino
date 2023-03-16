c6b2dfde56dec95291cf13f0e64e867ec7e0953b
sendust <67322016+sendust@users.noreply.github.com>
2023-03-16 18:09:18 +0900

    Add files via upload

---
+++ wifi_test_esp32.ino
@@ -0,0 +1,135 @@
+#include <WiFi.h>
+#include "time.h"
+
+int printwifistatus();
+void initWiFi();
+int buffer_push(int);
+void buffer_reset();
+void do_protocol(int);
+
+
+const char* ssid = "jungbi-5g";
+const char* password = "0221134820";
+
+unsigned long tm_now = millis();
+unsigned long tm_last_print = 0;
+unsigned long tm_last_wifiretry = 0;
+ 
+const char* wifistatus[] = {"idle", "no ssid avail", "scan complete", "connected", "fail connect", "connect lost", "disconnected"};
+
+
+const char* ntpServer = "pool.ntp.org";
+uint8_t timeZone = 9;
+uint8_t summerTime = 0; // 3600
+uint8_t sbuffer[20];
+uint8_t sbuffer_idx = 0;
+bool print_serial = false;
+
+void setup() {
+  Serial.begin(115200);
+  unsigned long tm_start = millis();
+  initWiFi();
+  Serial.print("WIFI connecting... ");
+  Serial.print(millis() - tm_start);
+  Serial.print("  mili seconds");
+  Serial.print("RRSI: ");
+  Serial.println(WiFi.RSSI());
+}
+
+void loop() {
+  int sbyte;
+  while(Serial.available())
+  {
+    sbyte = buffer_push(Serial.read());
+    print_serial = true;
+  }
+  if (print_serial)
+  {
+    for (int i = 0; i < 20; i++)
+    {
+      Serial.print(" ");
+      Serial.print(sbuffer[i]);
+    }
+    Serial.println("");
+    print_serial = false;
+  }
+
+  if (sbyte = 10)
+  {
+    do_protocol(sbuffer[0]);
+    buffer_reset();
+  }
+  
+  tm_now = millis();
+  if ((tm_now - tm_last_print) > 1000)
+  {
+    if ((printwifistatus() != WL_CONNECTED) && ((tm_now - tm_last_wifiretry) > 5000))
+      {
+        WiFi.begin(ssid, password);
+        Serial.println("Retry Wifi connection...");
+        tm_last_wifiretry = tm_now;
+      }
+    tm_last_print = tm_now;
+  }
+}
+
+
+
+void initWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+}
+
+int printwifistatus()
+{
+  int wstatus = WiFi.status();
+  Serial.println(wifistatus[wstatus]);
+  if (wstatus == WL_CONNECTED)
+  {
+    Serial.println(WiFi.localIP());
+    Serial.println(WiFi.RSSI());
+  }
+  return wstatus;
+}
+
+
+int buffer_push(int single_byte)
+{
+  sbuffer[sbuffer_idx] = single_byte;
+  if (sbuffer_idx < 19)
+  {
+    sbuffer_idx++;
+  }
+  else
+  {
+    sbuffer_idx = 0; 
+  }
+  return single_byte;
+}
+
+void buffer_reset()
+{
+  for (int i = 0; i < 20; i++)
+    sbuffer[i] = 0;
+  sbuffer_idx = 0;
+}
+
+
+void do_protocol(int cmd)
+{
+  if (cmd == 49)      // select number 1
+  {
+    Serial.println("You press number 1");
+  }
+
+  if (cmd == 50)      // select number 2
+  {
+    Serial.println("You press number 1");
+  }
+}
